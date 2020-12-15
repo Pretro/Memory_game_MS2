07350e046475086c7b3cd948910fcdfcccca753f
Pretro <jort2212@outlook.com>
2020-12-15 09:57:10 +0000

    Starting the making of the game board

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //GameCardArray
+    const gameCardArray = [ {name:'Covid-A', img:'images/covid19_A.jpg'},{name:'Pandemic', img:'images/pademic.jpg'} ]
+}
