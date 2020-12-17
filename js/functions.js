8ebf7830d0d6c3c5797c21d25fb0c11f2d1693ee
Pretro <jort2212@outlook.com>
2020-12-17 19:26:49 +0000

    Changeing the game board

--- js/functions.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    //GameCardArray
-    const gameCardArray = [ {name:'Covid-A', img:'images/covid19_A.jpg'},{name:'Pandemic', img:'images/pademic.jpg'} ]
-}
